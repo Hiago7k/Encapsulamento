12ccbbb9986a30053e4853eae52b5834f956859c
Hiago 7k <hiago7k@gmail.com>
2026-03-09 13:34:14 -0300

    criando nosso objeto

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Encapsulamento;
+
+Produto item1 = new Produto();
+item1.nome = "Teclado";
+item1.descricao = "Teclado mecanico para jogar";
+item1.preco = 80.00m;
+item1.estoque = 15;
+
+Console.WriteLine($"Dados do item: {item1.nome} {item1.descricao} {item1.preco} {item1.estoque}");
+
+item1.AlterarPrecoComDesconto(0.2m);
+
+
+
