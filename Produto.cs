1c0f3d4da49640d7a69a512e124e19a4fa399816
Hiago 7k <hiago7k@gmail.com>
2026-03-09 13:30:48 -0300

    criando classe, para implementar encapsulamento

---
+++ Produto.cs
@@ -0,0 +1,19 @@
+﻿namespace Encapsulamento;
+
+internal class Produto
+{
+    public string nome;
+    public string descricao;
+    public decimal preco;
+    public int estoque;
+
+    public bool EstaDisponivel()
+    {
+        return estoque > 0;
+    }
+
+    public void AlterarPrecoComDesconto(decimal desconto)
+    {
+        preco = preco * (1 - desconto / 100);
+    }
+}
