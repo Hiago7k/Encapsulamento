f5cc757d4efe26e884831f3a3ac497fef37b6c51
Hiago 7k <hiago7k@gmail.com>
2026-03-09 14:18:54 -0300

    criando ctor

--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
 ﻿using Encapsulamento;
 
 Produto item1 = new Produto();
-item1.nome = "Teclado";
-item1.descricao = "Teclado mecanico para jogar";
-item1.preco = 80.00m;
-item1.estoque = 15;
+item1.CriarProduto("Teclado", "mecanico", 80.00m, 12);
 
-Console.WriteLine($"Dados do item: {item1.nome} {item1.descricao} {item1.preco} {item1.estoque}");
 
 item1.AlterarPrecoComDesconto(0.2m);
 
